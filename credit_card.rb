ab6695b68747aeea265dfdf1f6aa86c3e1cc1906
rojotic26 <rojotic26@gmail.com>
2015-03-31 14:31:03 +0800

    Adding credit_card.rb

--- credit_card.rb
+++ credit_card.rb
@@ -1,3 +1,29 @@
 require_relative './luhn_validator.rb'
 require 'json'
+class CreditCard
+  include LuhnValidator
+  # instance variables with automatic getter/setter methods
+  attr_accessor :number, :expiration_date, :owner, :credit_network
 
+  def initialize(number, expiration_date, owner, credit_network)
+    @number = number
+    @expiration_date = expiration_date
+    @owner = owner
+    @credit_network = credit_network
+  end
+
+  # returns json string
+  def to_json
+    {
+      :number => number,
+      :expiration_date => expiration_date,
+      :owner => owner,
+      :credit_network => credit_network
+     }.to_json
+  end
+
+  # returns all card information as single string
+  def to_s
+    self.to_json
+  end
+end
---
+++ credit_card.rb~
@@ -0,0 +1,3 @@
+require_relative './luhn_validator.rb'
+require 'json'
+
